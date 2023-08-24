471f1d270cdc72de5d34f2387d61129043072b97
Lucas Figueroa <lucas_figueroa_11@hotmail.com>
2023-08-23 21:22:49 -0300

    agregamos una variable para guardar un puerto

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
-console.log("esta es una prueba 2ddddd");
+console.log("esta es una prueba 2ddddsdsssssssssssssd");
 
 // 1- configuraciones iniciales
-
+const app = express();
+//crear una variable en express
+app.set("port", process.env.PORT || 4000);
 // 2- middelwares
 
 // 3-Crear las rutas
